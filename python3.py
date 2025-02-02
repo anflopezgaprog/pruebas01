acfd0d8f360020b8b4e83e86467ef413f688d4ae
AndresLopez <anflopezga.prog@gmail.com>
2025-02-02 17:44:44 -0500

    Se crea la función suma en python 3

---
+++ python3.py
@@ -0,0 +1,14 @@
+def suma(num1, num2):
+    num1 = int(num1)#input("Número 1"))
+    print("El número 1 es", num1)
+    num2 = int(num2)#input("Número 2"))
+    print("El número 2 es", num2)
+    
+    sum = num1 + num2
+    print(sum)
+    return(sum)
+
+
+print(suma(3,2))
+
+print(3)
